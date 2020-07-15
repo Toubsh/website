335887d02af3fd92d0c46c271a14d65782f59bc1
Toubsh <Toubsh@hotmail.de>
2020-07-15 10:24:52 +0200

    Restructure complete layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,15 @@ import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
-import { WelcomeScreenComponent } from './welcome-screen/welcome-screen.component';
-import * as anime from 'animejs';
 import { HomeComponent } from './home/home.component';
 import { TabComponent } from './home/tab/tab.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeScreenComponent,
     HomeComponent,
     TabComponent,
-    DashboardComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/home/tab/tab.component.ts
+++ src/app/home/tab/tab.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class TabComponent implements OnInit {
 
-  @Input() tabName: string;
+  @Input() tab: string;
 
   constructor() { }
 
