0ece667d15215763a241a007d84ed4dc69ffa9d0
Toubsh <Toubsh@hotmail.de>
2020-07-16 16:09:02 +0200

    Create Welcome animation
    * Style tabs
    * Change Welcome animation to darker hues
    * Make lines not spread out vertically so much
    * Resize Welcome animation container to zero after animation ends

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
-import { gsap } from "gsap";
+import { gsap, random } from 'gsap';
 
 @Component({
   selector: 'app-home',
@@ -9,17 +9,31 @@ import { gsap } from "gsap";
 })
 export class HomeComponent implements OnInit {
 
-  tabs: String[] = ["Home", "About me", "My 'creations'", "Contact"];
+  tabs: string[] = ['Home', 'About me', 'My "creations"', 'Contact'];
 
-  constructor() { }
+  showWelcomeAnim: boolean = true;
+
+  @Output() event = new EventEmitter();
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.showWelcomeAnim = true;
   }
 
   ngAfterViewInit() {
-    let tl = gsap.timeline();
-    tl.to("#welcome-text", {duration: 1, opacity: 1});
-    tl.to("#welcome-text", {duration: 2, opacity: 0});
+    // Startup welcome animation when first visiting site
+    const tl = gsap.timeline();
+    tl.to('#welcome-text', {duration: 1, opacity: 1})
+      .to('#welcome-text', {duration: 0.8, opacity: -1.9})
+      .to('.anim-div-1', {duration: 0.7, x: gsap.utils.random(3000, 3500), backgroundColor: 'rgb(' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ')'}, '-=' + gsap.utils.random(0.6, 0.7))
+      .to('.anim-div-2', {duration: 1.1, x: gsap.utils.random(3000, 3500), backgroundColor: 'rgb(' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ')'}, '-=' + gsap.utils.random(0.8, 0.9))
+      .to('.anim-div-3', {duration: 0.7, x: gsap.utils.random(3000, 3500), backgroundColor: 'rgb(' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ')'}, '-=' + gsap.utils.random(1, 1.1))
+      .to('.anim-div-4', {duration: 1.1, x: gsap.utils.random(3000, 3500), backgroundColor: 'rgb(' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ')'}, '-=' + gsap.utils.random(1, 1.1))
+      .to('.anim-div-5', {duration: 0.7, x: gsap.utils.random(3000, 3500), backgroundColor: 'rgb(' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ')'}, '-=' + gsap.utils.random(1.1, 1.2))
+      .to('.anim-div-6', {duration: 1.1, x: gsap.utils.random(3000, 3500), backgroundColor: 'rgb(' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ')'}, '-=' + gsap.utils.random(1.1, 1.2))
+      .to('#home-anim-container', {width: 0, height: 0});
   }
 
 }
