6fc551219d5b048ed596647087ef3590c798f50f
Toubsh <Toubsh@hotmail.de>
2020-08-20 17:16:28 +0200

    * Tweak welcome animation * Make tab animation work after welcome animation ended

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, ViewChild } from '@angular/core';
 
-import { gsap, random } from 'gsap';
+import { gsap } from 'gsap';
+import { TabComponent } from './tab/tab.component';
 
 @Component({
   selector: 'app-home',
@@ -9,31 +10,41 @@ import { gsap, random } from 'gsap';
 })
 export class HomeComponent implements OnInit {
 
-  tabs: string[] = ['Home', 'About me', 'My "creations"', 'Contact'];
+  @Output() runningWelcomeAnim: boolean = false;
 
-  showWelcomeAnim: boolean = true;
+  @ViewChild(TabComponent)
+  public tab: TabComponent;
 
-  @Output() event = new EventEmitter();
+  tl = gsap.timeline({paused: true});
 
   constructor() {
   }
 
   ngOnInit() {
-    this.showWelcomeAnim = true;
   }
 
   ngAfterViewInit() {
     // Startup welcome animation when first visiting site
-    const tl = gsap.timeline();
-    tl.to('#welcome-text', {duration: 1, opacity: 1})
-      .to('#welcome-text', {duration: 0.8, opacity: -1.9})
-      .to('.anim-div-1', {duration: 0.7, x: gsap.utils.random(3000, 3500), backgroundColor: 'rgb(' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ')'}, '-=' + gsap.utils.random(0.6, 0.7))
-      .to('.anim-div-2', {duration: 1.1, x: gsap.utils.random(3000, 3500), backgroundColor: 'rgb(' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ')'}, '-=' + gsap.utils.random(0.8, 0.9))
-      .to('.anim-div-3', {duration: 0.7, x: gsap.utils.random(3000, 3500), backgroundColor: 'rgb(' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ')'}, '-=' + gsap.utils.random(1, 1.1))
-      .to('.anim-div-4', {duration: 1.1, x: gsap.utils.random(3000, 3500), backgroundColor: 'rgb(' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ')'}, '-=' + gsap.utils.random(1, 1.1))
-      .to('.anim-div-5', {duration: 0.7, x: gsap.utils.random(3000, 3500), backgroundColor: 'rgb(' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ')'}, '-=' + gsap.utils.random(1.1, 1.2))
-      .to('.anim-div-6', {duration: 1.1, x: gsap.utils.random(3000, 3500), backgroundColor: 'rgb(' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ')'}, '-=' + gsap.utils.random(1.1, 1.2))
-      .to('#home-anim-container', {width: 0, height: 0});
+    this.welcomeAnimLogic();
+    this.tl.play();
+  }
+
+  setHomeAnimToFalse() {
+    this.runningWelcomeAnim = true;
+  }
+
+  welcomeAnimLogic() {
+    // "Welcome" moving-row animation
+    return this.tl.to('#welcome-text', {duration: 1, opacity: 1})
+    .to('#welcome-text', {duration: 1.5, opacity: -0.5})
+    .to('.anim-div-1', {duration: 0.7, x: gsap.utils.random(3000, 3500), backgroundColor: 'rgb(' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ')'}, '-=' + gsap.utils.random(0.6, 0.7))
+    .to('.anim-div-2', {duration: 1.1, x: gsap.utils.random(3000, 3500), backgroundColor: 'rgb(' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ')'}, '-=' + gsap.utils.random(0.8, 0.9))
+    .to('.anim-div-3', {duration: 0.7, x: gsap.utils.random(3000, 3500), backgroundColor: 'rgb(' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ')'}, '-=' + gsap.utils.random(1, 1.1))
+    .to('.anim-div-4', {duration: 0.7, x: gsap.utils.random(3000, 3500), backgroundColor: 'rgb(' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ')'}, '-=' + gsap.utils.random(1, 1.1))
+    .to('.anim-div-5', {duration: 0.7, x: gsap.utils.random(3000, 3500), backgroundColor: 'rgb(' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ')'}, '-=' + gsap.utils.random(1.1, 1.2))
+    .to('.anim-div-6', {duration: 1.1, x: gsap.utils.random(3000, 3500), backgroundColor: 'rgb(' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ',' + gsap.utils.random(80, 100) + ')'}, '-=' + gsap.utils.random(1.1, 1.2))
+    .set('#home-anim-container', {width: 0, height: 0, visibility: 'hidden'})
+    .then(() => { this.tab.tabEntryAnim(); });
   }
 
 }
--- src/app/home/tab/tab.component.ts
+++ src/app/home/tab/tab.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-tab',
@@ -7,11 +9,24 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class TabComponent implements OnInit {
 
-  @Input() tab: string;
+  tabs: string[] = ['Home', 'About me', 'My "creations"', 'Contact'];
+
+  @Input() welcomeAnimFinished;
+
+  tl = gsap.timeline({paused: true});
 
   constructor() { }
 
   ngOnInit() {
+    this.welcomeAnimFinished = false;
+  }
+
+  afterViewInit() {
+  }
+
+  tabEntryAnim() {
+    this.tl.to('.tabs', {x: 0, stagger: 0.15});
+    this.tl.play();
   }
 
 }
